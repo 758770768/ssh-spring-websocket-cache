3d6ceb7370f1af521c8a441140adae65fc70e860
758770768 <758770768@qq.com>
2018-04-09 15:57:15 +0800

    新增文件上传

--- src/main/java/org/ssh02/web/EmpController.java
+++ src/main/java/org/ssh02/web/EmpController.java
@@ -6,12 +6,17 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.ssh02.pojo.Emp;
 import org.ssh02.pojo.User;
 import org.ssh02.service.EmpService;
 
 @Controller
+@CrossOrigin("*")
 public class EmpController {
 
 	@Resource
@@ -30,6 +35,13 @@ public class EmpController {
 		String sql = "select * from emp";
 		List<Emp> el = es.selectAll(sql, new Emp());
 		System.out.println(el);
+		return null;
+	}
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	@ResponseBody
+	public String fn2(MultipartFile mfile) {
+		System.out.println("ok");
 		return null;
 	}
 }
